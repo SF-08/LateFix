aee967a17fa0b53972d38fc75541e177dab0b812
Scott Fuller <shrgf1234@gmail.com>
2026-02-05 19:08:48 +0000

    .

--- latefix/src/utils/supabase.ts
+++ latefix/src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import 'react-native-url-polyfill/auto'
 import 'expo-sqlite/localstorage/install'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createClient, processLock } from '@supabase/superbase.js'
+import { createClient, processLock } from '@supabase/supabase-js'
 
 export const supabase = createClient(
     process.env.EXPO_PUBLIC_SUPABASE_URL!,
