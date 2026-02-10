cdadf497a07f2e32c02fec3806dbe401b523b566
Scott Fuller <shrgf1234@gmail.com>
2026-02-10 11:13:42 +0000

    Welcome Page - Work at Home

---
+++ latefix/app/src/components/auth/WelcomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import supabase from "../../utils/supabase";
+
+const WelcomePage: React.FC = () => {
+  const navigation = useNavigation();
+}
